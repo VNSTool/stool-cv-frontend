8a96df3e6ca25b45b114dc7d88a69d323bdb8ef4
Tri Nguyen <nmtri.dev@gmail.com>
2022-06-26 21:09:36 +0700

    deply onto Production to see if its able to send pageview hits

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -21,6 +21,15 @@ export default {
             "This page is Tri's curriculum vitae. You can find my experiences and skills here, also hire me by share me jod details.",
         },
       ],
+      script: [
+        {
+          src: "https://www.googletagmanager.com/gtag/js?id=G-CMVXBBK798",
+          async: true,
+        },
+        {
+          src: "/js/gtag-setting.js",
+        },
+      ],
     };
   },
 };
