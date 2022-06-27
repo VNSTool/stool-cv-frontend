4f64d802be34c97a3ae68dacea0282a0a3eee1b1
Tri Nguyen <nmtri.dev@gmail.com>
2022-06-27 15:51:06 +0700

    try google analytics

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -23,5 +23,13 @@ export default {
       ],
     };
   },
+  mounted() {
+    // this.$ga.page(this.$router);
+    this.$ga.page({
+      page: "/",
+      title: "Home page",
+      location: window.location.href,
+    });
+  },
 };
 </script>
