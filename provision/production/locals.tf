e6d6cedb6c76dbb3d6dc976e377489287040e467
Tri Nguyen <57081360+nmtri-dev@users.noreply.github.com>
2022-08-05 21:24:51 +0700

    STOOL-15: Use t4g.small for frontend (#18)

--- provision/production/locals.tf
+++ provision/production/locals.tf
@@ -19,7 +19,7 @@ locals {
   public_cidr_block_a         = "10.0.0.0/28"
   public_cidr_block_b         = "10.0.0.16/28"
 
-  instance_type               = "t3a.micro"
+  instance_type               = "t4g.small"
   spot_price                  = "0.01"
   ec2_role                    = "stool-ec2-curriculum-vitae-production"
   min_size                    = 1
